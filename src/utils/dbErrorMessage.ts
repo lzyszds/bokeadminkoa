19a397e5c617dc352e2ee1aad9d651a7706d47b2
lzyszds <1024327189@qq.com>
2024-01-18 19:36:26 +0800

    新增sql语句错误提示中文

---
+++ src/utils/dbErrorMessage.ts
@@ -0,0 +1,10 @@
+//一些数据库错误的中文提示
+export const dbErrorMessage: any = {
+    'ER_DUP_ENTRY': '数据库中已存在该记录',
+    'ER_NO_REFERENCED_ROW': '外键约束检查失败',
+    'ER_NO_REFERENCED_ROW_2': '外键约束检查失败',
+    'ER_ROW_IS_REFERENCED': '外键约束检查失败',
+    'ER_ROW_IS_REFERENCED_2': '外键约束检查失败',
+    'ER_DUP_ENTRY_WITH_KEY_NAME': '数据库中已存在该记录',
+    'ER_DUP_ENTRY_WITH_UNIQUE_KEY': '数据库中已存在该记录',
+}
