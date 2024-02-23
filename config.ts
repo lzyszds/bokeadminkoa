b42bee8da9296bb6fb7fdbfe3171a5dd46bbe443
lzyszds <1024327189@qq.com>
2024-02-23 22:16:51 +0800

    ✨ feat: 新增env配置

--- config.ts
+++ config.ts
@@ -9,6 +9,8 @@ export default {
     staticDir: path.resolve('./public'), // 静态资源路径
     uploadDir: path.join(__dirname, path.resolve('public/')), // 上传文件路径
     weatherKey: "78182b9b39355dc0ae4ce91dae7f0bbf",
+    //根据当前文件名字的后缀名字来判断当前是开发环境还是生产环境 开发环境为.ts 生产环境为.js
+    env: path.extname(__filename) === '.ts' ? 'development' : 'production',
     // 数据库连接设置
     dbConfig: {
         connectionLimit: 10,
@@ -24,6 +26,7 @@ export default {
         '/user/getRandHeadImg',
         '/article/getRandArticleImg',
         '/article/getArticleList',
+        '/article/getArticleComment',
         '/article/getArticleInfo',
         '/article/getArticleTypeList',
         '/common/ipConfig'
